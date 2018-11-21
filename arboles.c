824faca7d3f2574501d473631c85d5546bed13fa
Rafael Garcia <regestra64@gmail.com>
2018-11-21 15:32:56 -0600

    funcionabilidad de eliminar agregada

--- arboles.c
+++ arboles.c
@@ -6,15 +6,17 @@ typedef struct nodo
     int valor;
     struct nodo *izdo;
     struct nodo *drcho;
+    struct nodo *padre;
 } Nodo;
 
 typedef Nodo arbol;
 
-Nodo *CrearNodo(int valor)
+Nodo *CrearNodo(int valor, Nodo *padre)
 {
     Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
     nuevoNodo->valor = valor;
     nuevoNodo->izdo = nuevoNodo->drcho = NULL;
+    nuevoNodo->padre = padre;
     return nuevoNodo;
 }
 
@@ -23,24 +25,30 @@ void DestruirNodo(Nodo *nodo)
     nodo->izdo = nodo->drcho = NULL;
     free(nodo);
 }
-void Insertar(Nodo **arbol, int valor)
+
+static void InsertarConPadre(Nodo **arbol, Nodo *padre, int valor)
 {
     if (*arbol == NULL)
     {
-        *arbol = CrearNodo(valor);
+        *arbol = CrearNodo(valor, padre);
     }
     else
     {
         int valorRaiz = (*arbol)->valor;
         if (valor < valorRaiz)
         {
-            Insertar(&(*arbol)->izdo, valor);
+            InsertarConPadre(&(*arbol)->izdo, *arbol, valor);
         }
         else
         {
-            Insertar(&(*arbol)->drcho, valor);
+            InsertarConPadre(&(*arbol)->drcho, *arbol, valor);
         }
     }
+}
+
+void Insertar(Nodo **arbol, int valor)
+{
+    InsertarConPadre(arbol, NULL, valor);
 }
 
 int Existe(Nodo *arbol, int valor)
@@ -63,15 +71,109 @@ int Existe(Nodo *arbol, int valor)
     }
 }
 
-void DeterminarExistencia(Nodo *arbol, int valor)
+Nodo *Obtener(Nodo *arbol, int valor)
 {
-    if (Existe(arbol, valor))
+
+    if (arbol == NULL)
     {
-        printf("El nodo %d existe en el arbol.\n", valor);
+        return NULL;
+    }
+    else if (arbol->valor == valor)
+    {
+        return arbol;
+    }
+    else if (valor < arbol->valor)
+    {
+        return Obtener(arbol->izdo, valor);
     }
     else
     {
-        printf("El nodo %d No existe en el arbol.\n", valor);
+        return Obtener(arbol->drcho, valor);
+    }
+}
+
+static void Reemplazar(Nodo *arbol, Nodo *nuevoNodo)
+{
+    if (arbol->padre)
+    {
+        if (arbol->valor == arbol->padre->izdo->valor)
+        {
+            arbol->padre->izdo = nuevoNodo;
+        }
+        else if (arbol->valor == arbol->padre->drcho->valor)
+        {
+            arbol->padre->drcho = nuevoNodo;
+        }
+        if (nuevoNodo)
+        {
+            nuevoNodo->padre = arbol->padre;
+        }
+    }
+}
+
+static Nodo *Minimo(Nodo *arbol)
+{
+    if (arbol == NULL)
+    {
+        return NULL;
+    }
+    if (arbol->izdo)
+    {
+        return Minimo(arbol->izdo);
+    }
+    else
+    {
+        return arbol;
+    }
+}
+
+static void EliminarNodo(Nodo *nodoEliminar)
+{
+
+    if (nodoEliminar->izdo && nodoEliminar->drcho)
+    {
+        //Tratar elimiar con 2 hijos
+        Nodo *minimo = Minimo(nodoEliminar->drcho);
+        nodoEliminar->valor = minimo->valor;
+        EliminarNodo(minimo);
+    }
+    else if (nodoEliminar->izdo)
+    {
+        //Tratar eliminar con 1 hijo
+        Reemplazar(nodoEliminar, nodoEliminar->izdo);
+        DestruirNodo(nodoEliminar);
+    }
+    else if (nodoEliminar->drcho)
+    {
+        //Tratar eliminar con 1 hijo
+        Reemplazar(nodoEliminar, nodoEliminar->drcho);
+        DestruirNodo(nodoEliminar);
+    }
+    else
+    {
+        //Tratar eliminar con 0 hijos
+        Reemplazar(nodoEliminar, NULL);
+        DestruirNodo(nodoEliminar);
+    }
+}
+
+void Eliminar(Nodo *arbol, int valor)
+{
+    if (arbol == NULL)
+    {
+        return;
+    }
+    else if (valor < arbol->valor)
+    {
+        Eliminar(arbol->izdo, valor);
+    }
+    else if (valor > arbol->valor)
+    {
+        Eliminar(arbol->drcho, valor);
+    }
+    else
+    {
+        EliminarNodo(arbol);
     }
 }
 
@@ -126,12 +228,16 @@ void Postorden(Nodo *arbol)
 int main(void)
 {
     Nodo *arbol = NULL;
-    Insertar(&arbol, 5);    Postorden(arbol); printf("\n");
-    Insertar(&arbol, 10);   Postorden(arbol); printf("\n");
-    Insertar(&arbol, 4);    Postorden(arbol); printf("\n");
-    Insertar(&arbol, 9);    Postorden(arbol); printf("\n");
-    Insertar(&arbol, 15);   Postorden(arbol); printf("\n");
-    //DeterminarExistencia(arbol, 10);
-    //DeterminarExistencia(arbol, 3);
-    //DeterminarExistencia(arbol, 9);
+    /*Insertar(&arbol, 5);    Inorden(arbol); printf("\n");
+    Insertar(&arbol, 10);   Inorden(arbol); printf("\n");
+    Insertar(&arbol, 4);    Inorden(arbol); printf("\n");
+    Insertar(&arbol, 9);    Inorden(arbol); printf("\n");
+    Insertar(&arbol, 15);   Inorden(arbol); printf("\n");
+    Insertar(&arbol, 20);   Inorden(arbol); printf("\n");
+    Insertar(&arbol, 2);    Inorden(arbol); printf("\n");
+
+    Eliminar(arbol,4);  Inorden(arbol); printf("\n");
+    Eliminar(arbol,15);  Inorden(arbol); printf("\n");
+    Eliminar(arbol,5);  Inorden(arbol); printf("\n");
+    */
 }
